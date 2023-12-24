5e344959dca62810ce47c5fc582c80ee06ff997d
SpaceBoy <jd.spaceguy@gmail.com>
2023-12-24 17:39:38 -0400

    Update (esta sí)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Wellcome from './components/welcome/Welcome'
+
+function App() {
+  return (
+    <>
+    <Wellcome />
+    </> 
+  )
+}
+
+export default App
