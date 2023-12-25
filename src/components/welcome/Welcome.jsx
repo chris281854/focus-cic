e64618fdf9b79d54151cc0d3f0cbdb706ad06b61
SpaceBoy <jd.spaceguy@gmail.com>
2023-12-25 19:25:23 -0400

    Avance en el diseño de la página de bienvenida

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -15,21 +15,36 @@ export default function Wellcome() {
             </div>
           </div>
           <div>
-          <a href="">log in</a>
-          <a href="">Register</a>
+            <a href="">log in</a>
+            <a href="">Register</a>
           </div>
         </header>
         <div className="container">
-        <div className="title-box">
-          <h1>This, is Focus</h1>
+          <div className="box">
+            <h1>This, is Focus</h1>
+          </div>
+          <br />
+          <div className="text-block">
+            Focus Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Quibusdam repellendus quae ea et, officiis dolores nihil asperiores
+            est, sapiente dignissimos laudantium. Quod temporibus deleniti quae
+            impedit expedita ex et earum?
+          </div>
+          <div className="cards">
+            <div className="card">1</div>
+            <div className="card">2</div>
+            <div className="card">3</div>
+            <div className="card">4</div>
+          </div>
         </div>
-        Focus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam repellendus quae ea et, officiis dolores nihil asperiores est, sapiente dignissimos laudantium. Quod temporibus deleniti quae impedit expedita ex et earum?
       </div>
-        </div>
       <footer className="footer-welcome">
-        <p>Yes... Focus</p>
-
-        <p>JD</p>
+        <p>FOCUS PROJECT</p>
+        <div id="footer-links">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact Us</a>
+        </div>
       </footer>
     </>
   )
