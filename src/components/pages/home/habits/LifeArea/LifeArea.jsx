0e59030376724ed702279d67a2a2c1215219296e
SpaceBoy <jd.spaceguy@gmail.com>
2024-09-18 14:44:42 -0400

    "Refactored LifeArea component: added week goal input, rearranged layout, and updated styles"

--- src/components/pages/home/habits/LifeArea/LifeArea.jsx
+++ src/components/pages/home/habits/LifeArea/LifeArea.jsx
@@ -33,6 +33,8 @@ export default function LifeArea({
   const satisfaction = area.scores[0]?.score_value
   const [newSatisfaction, setNewSatisfaction] = useState(satisfaction)
   const [longTermGoal, setLongTermGoal] = useState(area.long_goal)
+  const [weekGoal, setWeekGoal] = useState(area.goals[0])
+
   const relatedGoals = []
   const [areaName, setAreaName] = useState(area.name)
   const [scoreBar, setScoreBar] = useState(satisfaction * 10)
@@ -104,7 +106,7 @@ export default function LifeArea({
       <div
         className="flex bg-bg-main-color flex-col justify-start p-4 w-full sm:w-full md:w-11/12 lg:w-11/12 h-full sm:h-5/6 rounded-3xl mx-auto my-auto overflow-scroll no-scrollbar"
         onClick={handleChildClick}>
-        <div className="flex p-4 mb-4 shadow-md rounded-md bg-primary/75 dark:bg-slate-800 hover:bg-slate-900 transition-all items-center justify-between">
+        <div className="flex p-4 mb-4 shadow-md rounded-md bg-primary/75 dark:bg-slate-900 hover:bg-slate-900 transition-all items-center justify-between">
           <input
             type="text"
             className={`w-72 mr-3 text-2xl font-bold text-white !bg-transparent`}
@@ -136,74 +138,86 @@ export default function LifeArea({
             <label>{newSatisfaction}</label>
           </div>
         </div>
-        <div className="flex p-2 mb-4 shadow-md rounded-md bg-primary/75 dark:bg-slate-800 hover:bg-slate-900 transition-all items-center flex-col w-80 h-fit max-h-72">
-          <label htmlFor="long_goal" className="text-xl font-semibold mb-1">
-            Metas
-          </label>
-          <textarea
-            name="long_goal"
-            id="long_goal"
-            className="text-xl h-full w-full min-h-20 font-thin mb-1 text-white bg-transparent scrollbar-none"
-            onChange={(e) => setLongTermGoal(e.target.value)}
-            value={longTermGoal}></textarea>
+        <div className="flex gap-1 flex-wrap">
+          <Card className="bg-primary/75 dark:bg-slate-800 hover:bg-slate-900 transition-all w-80 h-fit max-h-72">
+            <CardHeader>
+              <label htmlFor="long_goal" className="text-xl font-semibold">
+                Metas
+              </label>
+            </CardHeader>
+            <CardContent>
+              <textarea
+                name="long_goal"
+                id="long_goal"
+                className="h-full w-full min-h-20 font-thin text-white bg-transparent scrollbar-none"
+                onChange={(e) => setLongTermGoal(e.target.value)}
+                value={longTermGoal}></textarea>
+            </CardContent>
+          </Card>
+          <Card className="bg-primary/75 dark:bg-slate-800 hover:bg-slate-900 transition-all w-80 h-fit max-h-72">
+            <CardHeader>
+              <label htmlFor="week_goal">Objetivos de la semana</label>
+            </CardHeader>
+            <CardContent>
+              <textarea
+                name="week_goal"
+                id="week_goal"
+                className="h-full w-full min-h-20 font-thin text-white bg-transparent scrollbar-none"
+                onChange={(e) => setWeekGoal(e.target.value)}
+                value={weekGoal}></textarea>
+            </CardContent>
+          </Card>
+          <Card className={"shadow-md bg-slate-900 transition-all w-4/12"}>
+            <CardHeader>
+              <CardTitle>Seguidor de Satisfacción</CardTitle>
+              <CardDescription>Valoraciones del último mes</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ChartContainer
+                config={chartConfig}
+                className="min-h-[200px] w-full">
+                <LineChart
+                  margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
+                  accessibilityLayer
+                  data={chartData}>
+                  <CartesianGrid vertical={false} />
+                  <XAxis
+                    dataKey="Fecha"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={8}
+                  />
+                  <YAxis type="number" domain={[0, 10]} />
+                  <Tooltip />
+                  <Line
+                    dataKey="Satisfacción"
+                    type="natural"
+                    stroke="var(--color-desktop)"
+                    strokeWidth={2}
+                    dot={({ payload, index, ...props }) => {
+                      return (
+                        <Dot
+                          key={`${payload.Fecha}-${index}`}
+                          r={5}
+                          cx={props.cx}
+                          cy={props.cy}
+                          fill="var(--color-desktop)"
+                          stroke="var(--color-desktop)"
+                        />
+                      )
+                    }}
+                  />
+                  <Legend />
+                </LineChart>
+              </ChartContainer>
+            </CardContent>
+            <CardFooter>
+              <div className="flex w-full items-start gap-2 text-sm">
+                {dayjs().format("MMM YYYY")}
+              </div>
+            </CardFooter>
+          </Card>
         </div>
-        {relatedGoals &&
-          relatedGoals.map((goal, index) => (
-            <div key={index}>
-              <p className="text-gray-300">{goal}</p>
-            </div>
-          ))}
-
-        <Card className={"shadow-md bg-slate-900 transition-all w-4/12"}>
-          <CardHeader>
-            <CardTitle>Seguidor de Satisfacción</CardTitle>
-            <CardDescription>Valoraciones del último mes</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <ChartContainer
-              config={chartConfig}
-              className="min-h-[200px] w-full">
-              <LineChart
-                margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
-                accessibilityLayer
-                data={chartData}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="Fecha"
-                  tickLine={false}
-                  axisLine={false}
-                  tickMargin={8}
-                />
-                <YAxis type="number" domain={[0, 10]} />
-                <Tooltip />
-                <Line
-                  dataKey="Satisfacción"
-                  type="natural"
-                  stroke="var(--color-desktop)"
-                  strokeWidth={2}
-                  dot={({ payload, index, ...props }) => {
-                    return (
-                      <Dot
-                        key={`${payload.Fecha}-${index}`}
-                        r={5}
-                        cx={props.cx}
-                        cy={props.cy}
-                        fill="var(--color-desktop)"
-                        stroke="var(--color-desktop)"
-                      />
-                    )
-                  }}
-                />
-                <Legend />
-              </LineChart>
-            </ChartContainer>
-          </CardContent>
-          <CardFooter>
-            <div className="flex w-full items-start gap-2 text-sm">
-              {dayjs().format("MMM YYYY")}
-            </div>
-          </CardFooter>
-        </Card>
         <div className="">{areaHasEvents && generateEvents(areaEvents)}</div>
       </div>
     </div>
